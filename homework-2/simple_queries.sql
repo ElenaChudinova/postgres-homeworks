0879873ece5933ebf42a677da515413228b065d4
ElenaChudinova <maknali@yandex.ru>
2023-11-17 16:49:19 +0300

    update files

--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -1,16 +1,16 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. "имя контакта" и "город" (contact_name, city) из таблицы customers (только эти две колонки)
-SELECT contact_name, city FROM customers
+SELECT contact_name, city FROM customers;
 
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
-SELECT order_id, shipped_date - order_date as diff_date FROM orders
+SELECT order_id, shipped_date - order_date as diff_date FROM orders;
 
 -- 3. все города без повторов, в которых зарегистрированы заказчики (customers)
-SELECT DISTINCT city FROM customers
+SELECT DISTINCT city FROM customers;
 
 -- 4. количество заказов (таблица orders)
-SELECT COUNT(*) FROM orders
+SELECT COUNT(*) FROM orders;
 
 -- 5. количество стран, в которые отгружался товар (таблица orders, колонка ship_country)
 SELECT ship_country, COUNT(*) FROM orders
-GROUP BY ship_country
+GROUP BY ship_country;
